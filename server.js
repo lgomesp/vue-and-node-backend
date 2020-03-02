6228817e0ab9c1d72b27e82ecd83f3b134f6d65f
Lucas Gomes <lucasgomesp22@gmail.com>
2020-03-02 17:59:17 -0300

    add simple authentication

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-var messages = ["hello", "hi", "working"];
+var messages = [{user: "Jim", text: "hello"}, {user: "Jim", text: "hi"}];
+var users = [{userName: "Jim", userPassword: "1"}];
 
 app.get('/messages', (req, res) => {
     res.send(messages);
@@ -18,11 +19,18 @@ app.get('/messages/:id', (req, res) => {
 });
 
 app.post('/messages', (req, res) => {
-    let msg = req.body;
-    console.log(req.body);
-    messages.push(msg.message);
+    const userId = req.header('Authorization');
+    const user = users[userId];
+    let msg = { user: user.userName, text: req.body.message };
+    messages.push(msg);
     res.json(msg);
-    console.log(messages);
+});
+
+app.post('/register', (req, res) => {
+    let registerData = req.body;
+    let newIndex = users.push(registerData);
+    registerData.id = newIndex - 1;
+    res.json(registerData);
 });
 
 app.listen(port, () => console.log('app running'));
